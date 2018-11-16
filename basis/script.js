e5e46e10b13aa52ce029a2133462671a4b84651a
Joey Westenberg <jojowestenberg@gmail.com>
2018-11-16 18:25:20 +0100

    les 6 v1

--- basis/script.js
+++ basis/script.js
@@ -2,7 +2,7 @@
 
 // Declare variables
 const studentName = "Joey Westenberg";
-const studentGroup = "MD2B";
+const studentGroup = "MD2A";
 const year = main.year;
 
 // Init
@@ -12,7 +12,21 @@ init();
 
 function init() {
     // Set content based on variables
-    document.getElementById("studentName").innerText = studentName;
+    var nameClass = document.getElementsByClassName("studentName");
+    for(var i = 0; i < nameClass.length; i++){
+        nameClass[i].innerText = studentName;    // Change the content
+    }
+
+    var groupClass = document.getElementsByClassName("studentGroup");
+    for(var i = 0; i < groupClass.length; i++){
+        groupClass[i].innerText = studentGroup;    // Change the content
+    }
+
+    var yearClass = document.getElementsByClassName("pageYear");
+    for(var i = 0; i < yearClass.length; i++){
+        yearClass[i].innerText = year;    // Change the content
+    }
+
     // Add navigation link to the navigation panel on the left of the page
     main.addContenItem("Inleiding", intro);
     // Activate the first navigation link
@@ -20,6 +34,7 @@ function init() {
 }
 
 function intro() {
+
     let description = "Je bent in dienst van energiemaatschappij Energy Solutions en"
         + " krijgt de opdracht om een webpagina te maken die de energiestanden toont van een aantal adressen."
         + " De data wordt via een webAPI beschikbaar gesteld. "
